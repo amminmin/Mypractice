3c291616e0dd404cd226d9b0fc2451a6c2f590d9
min <isminzhao187@gmail.com>
2025-09-12 12:02:26 +0900

    修改了设置文件，将屏幕宽度、高度和背景颜色等配置移到了一个单独的Settings类中

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,5 +1,6 @@
 import sys
 import pygame
+from settings import Settings 
 
 class AlienInvasion:
     """管理游戏资源和行为的类"""
@@ -8,10 +9,14 @@ class AlienInvasion:
         """初始化游戏并创建游戏资源"""
         pygame.init()
         self.clock = pygame.time.Clock()
-        self.screen = pygame.display.set_mode((1200,600))
+        
+        self.settings = Settings()
+        
+        self.screen = pygame.display.set_mode(
+            (self.settings.screen_width, self.settings.screen_height))
         pygame.display.set_caption("Alien Invasion")
         
-        self.bg_color = (230, 230, 230)
+        self.bg_color = (self.settings.bg_color)
     
     def run_game(self):
         """开始游戏的主循环"""
