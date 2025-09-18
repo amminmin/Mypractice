2c3f59a129fb1c620afa4c73657fe126b7ca0022
min <isminzhao187@gmail.com>
2025-09-18 11:34:23 +0900

    增加左右移动功能

--- alien_invasion.py
+++ alien_invasion.py
@@ -37,8 +37,14 @@ class AlienInvasion:
             elif event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_RIGHT:
                     self.ship.moving_right = True
+                elif event.key == pygame.K_LEFT:
+                    self.ship.moving_left = True
             elif event.type == pygame.KEYUP:  
-                self.ship.moving_right = False     
+                if event.key == pygame.K_RIGHT:
+                    self.ship.moving_right = False  
+                elif event.key == pygame.K_LEFT:
+                    self.ship.moving_left = False
+  
                                     
     def _update_screen(self):
         """更新屏幕上的图像，并切换到新屏幕"""
